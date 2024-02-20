8db2527edc091895277b2040917e42bcbf774c5f
amineneji <amineneji1996@gmail.com>
2024-02-20 12:40:57 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'section': "url('C:\Users\ASUS\OneDrive\Bureau\tailwindd\src\background.jpeg')",
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
